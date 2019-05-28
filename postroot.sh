d3236632daf379126328925d40b2d35aeb5a5d11
Gijs Key <gmkey@users.noreply.github.com>
2019-05-28 15:09:47 +0200

    update post root.sh with latest portainer version

--- postroot.sh
+++ postroot.sh
@@ -86,10 +86,10 @@ then
 	fi
 
 	# pull portainer docker image
-	docker pull portainer/portainer:1.19.1
+	docker pull portainer/portainer:latest
 
 	# start portainer container
-	docker run --volume=/var/run/docker.sock:/var/run/docker.sock --volume=/opt/portainer:/data -p=9000:9000 --name="portainer" --restart="unless-stopped" --detach=true portainer/portainer:1.19.1
+	docker run --volume=/var/run/docker.sock:/var/run/docker.sock --volume=/opt/portainer:/data -p=9000:9000 --name="portainer" --restart="unless-stopped" --detach=true portainer/portainer:latest
 fi
 
 # Exit with Status 0
